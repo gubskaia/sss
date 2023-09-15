cd7721050e6fd5174996fe55e849c5be9d8a35dd
daria <gybskaya.dd@gmail.com>
2023-09-15 22:11:24 +0600

    commmmit 2

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("hhhheeellooo");
         System.out.println("hello2");
+        System.out.println("hello3");
     }
 }
